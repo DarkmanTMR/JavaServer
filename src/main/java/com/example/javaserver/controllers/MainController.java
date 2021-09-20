5ecb4e47028b1994fdacfaf1dcccdebb184e728e
DarkmanTMR <trp1prt1@gmail.com>
2021-09-21 01:04:58 +0300

    upload

--- src/main/java/com/example/javaserver/controllers/MainController.java
+++ src/main/java/com/example/javaserver/controllers/MainController.java
@@ -1,39 +1,79 @@
 package com.example.javaserver.controllers;
 
 
-
 import com.example.javaserver.dao.ClientDAO;
 import com.example.javaserver.models.Client;
-import org.springframework.web.bind.annotation.*;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
 
 @RestController
-//@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:4200"})
+@AllArgsConstructor
 public class MainController {
 
+    private ClientDAO clientDAO;
+
+//    @PostMapping("/upload")
+//    public void upload(MultipartFile file){
+//        System.out.println((file.getOriginalFilename()));
+//        System.out.println(System.getProperty("user.home"));
+//        String pathToSaveImage = System.getProperty("user.home") + File.separator + "Desktop" + File.separator;
+//        try {
+//            file.transferTo(new File(pathToSaveImage + file.getOriginalFilename()));
+//        } catch (IOException e){
+//            e.printStackTrace();
+//        }
 
 
+//        @PostMapping("/upload")
+//                public void upload(@RequestParam String name, MultipartFile file){
+//            System.out.println(name);
+//        }
 
+//    @PostMapping("/upload")
+//    public void upload(@RequestParam String name, MultipartFile file) {
+//
+//        String pathToSaveImage = System.getProperty("user.home") + File.separator + "Desktop" + File.separator + file.getOriginalFilename();
+//        try {
+//            file.transferTo(new File(pathToSaveImage));
+//            Client s = new Client();
+//            s.setName(name);
+//            s.setAvatar(pathToSaveImage);
+//            clientDAO.save(s);
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
 
-        private ClientDAO clientDAO;
-    public  MainController(ClientDAO clientDAO){
-        this.clientDAO = clientDAO;
-    }
+    @PostMapping("/upload")
+    public void upload(@RequestParam String name, MultipartFile file) {
 
+           try {
+            file.transferTo(new File(System.getProperty("user.home") + File.separator + "Desktop" + File.separator + file.getOriginalFilename()));
+            Client s = new Client();
+            s.setName(name);
+            s.setAvatar(file.getOriginalFilename());
+            clientDAO.save(s);
 
-        @GetMapping("/clients")
-    public List<Client> clients() {
-        List<Client> all = clientDAO.findAll();
-        return all;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-    @PostMapping("/client")
-    public void saveClient(@RequestBody Client client) {
-        System.out.println(client);
-        clientDAO.save(client);
-
     }
+
+
+
+
+
+
 }
+
 
 
--- src/main/java/com/example/javaserver/models/Client.java
+++ src/main/java/com/example/javaserver/models/Client.java
@@ -1,45 +1,26 @@
 package com.example.javaserver.models;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
+    private String avatar;
 
-    public Client() {
-    }
-
-    public Client(String name) {
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Client{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
